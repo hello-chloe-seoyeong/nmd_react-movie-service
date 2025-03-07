b5e8d7e5b48e49a9f727146bf7476eaf65dccad7
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-03-06 21:31:03 -0800

    Exercise useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import Button from "./Button";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
     <div>
-      <h1>Welcome</h1>
-      <Button text={"OK"} />
+      <h1>{counter}</h1>
+      <Button text={"OK"} onClick={onClick} />
     </div>
   );
 }
