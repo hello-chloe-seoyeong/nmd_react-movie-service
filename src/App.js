5c1a4d7b0c47c945ade845c9066f2d35e8483f90
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-03-09 12:01:02 -0700

    Exercise useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import { useEffect, useState } from "react";
-import Button from "./Button";
+
+function Hello() {
+  useEffect(() => {
+    console.log("Created :)");
+    return () => console.log("Destroyed :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
 
-  console.log("I run all the time...");
-
-  useEffect(() => {
-    console.log("I run only once");
-  }, []);
-  useEffect(() => {
-    if (keyword !== "") {
-      console.log("I run when 'keyword' changes.");
-    }
-  }, [keyword]); // [지켜볼 state, 콤마로 여러개 가능]
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
   return (
     <div>
-      <input type="text" value={keyword} onChange={onChange} />
-      <h1>{counter}</h1>
-      <Button text={"OK"} onClick={onClick} />
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
